553cdb3a729fc7c22a771cdaeb969f354f31c164
Bonghak Lee <bolee@c2r6s2.42seoul.kr>
2022-05-15 13:44:57 +0900

    refactoring application using module

--- application/index.js
+++ application/index.js
@@ -2,37 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var url = require('url');
 var qs = require('querystring');
-
-var template = {
-    HTML: function (title, list, description, control) {
-        return `
-        <!doctype html>
-        <html>
-        <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-        </head>
-        <body>
-            <h1><a href="/">WEB</a></h1>
-            ${list}
-            ${control}
-            <h2>${title}</h2>
-            ${description}
-        </body>
-        </html>
-        `;
-    },
-    list: function (filelist) {
-        var list = '<ul>';
-    
-        for (var i = 0; i < filelist.length; i++) {
-            list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`;
-        }
-        list += '</ul>';
-    
-        return list;
-    }
-}
+var template = require('./lib/template.js');
 
 var app = http.createServer((request, response) => {
     var _url = request.url;
